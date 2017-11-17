2df054c5e4e58322b68c4c93f7ec9aaca9105ccf
longxy <1871954195@qq.com>
2017-11-17 23:02:55 +0800

    订餐系统基础

--- db/01.init_mysql5.7.sql
+++ db/01.init_mysql5.7.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE if not exists `wxmp` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE DATABASE if not exists `dcxt` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 use mysql;
 
-CREATE USER 'wxmp'@'localhost' IDENTIFIED BY 'wxmp' PASSWORD EXPIRE NEVER;
+CREATE USER 'dcxt'@'localhost' IDENTIFIED BY 'dcxt' PASSWORD EXPIRE NEVER;
 
 UPDATE user SET authentication_string= password ('wxmp68') WHERE user='root';
 
@@ -12,8 +12,8 @@ grant all privileges on *.* to 'root'@'%' identified by 'wxmp68';
 grant all privileges on *.* to 'root'@'localhost' identified by 'wxmp68';
 grant all privileges on *.* to 'root'@'127.0.0.1' identified by 'wxmp68';
 
-grant all privileges on wxmp.* to 'wxmp'@'%' identified by 'wxmp';
-grant all privileges on wxmp.* to 'wxmp'@'localhost' identified by 'wxmp';
-grant all privileges on wxmp.* to 'wxmp'@'127.0.0.1' identified by 'wxmp';
+grant all privileges on dcxt.* to 'dcxt'@'%' identified by 'dcxt';
+grant all privileges on dcxt.* to 'dcxt'@'localhost' identified by 'dcxt';
+grant all privileges on dcxt.* to 'dcxt'@'127.0.0.1' identified by 'dcxt';
 
 flush privileges;
